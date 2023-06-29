6ccf40f39b9c31460f2c4ab7a94e8948a90ba003
Main <emanuelcalvopizarro0123@gmail.com>
2023-06-28 18:03:13 -0600

    Persona ya hecha

--- Examen1ECP/src/examen1ecp/Persona.java
+++ Examen1ECP/src/examen1ecp/Persona.java
@@ -11,6 +11,29 @@ package examen1ecp;
 public class Persona {
     public int cedula;
     public String Nombre;
-    
+    public String Apellido1;
+    public String A2;
+    public String Correo;
+    public String Descripcion;
+    public int edad;
+
+    public Persona(int cedula, String Nombre, String Apellido1, String A2, String Correo, String Descripcion, int edad) {
+        this.cedula = cedula;
+        this.Nombre = Nombre;
+        this.Apellido1 = Apellido1;
+        this.A2 = A2;
+        this.Correo = Correo;
+        this.Descripcion = Descripcion;
+        this.edad = edad;
+    }
+    public Persona(){
+        this.A2="";
+        this.Apellido1 = "";
+        this.Correo = "";
+        this.Descripcion = "";
+        this.Nombre = "";
+        this.cedula = 0;
+        this.edad = 0;
+    }
     
 }
